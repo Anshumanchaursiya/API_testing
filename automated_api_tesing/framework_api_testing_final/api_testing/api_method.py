41e371a302a3fc04ae295fd070f23706b80134a3
Anshumanchaursiya <ansh9010@gmail.com>
2019-06-10 01:01:30 -0700

    userId can be pass by user for every api requests in cms api

--- automated_api_tesing/framework_api_testing_final/api_testing/api_method.py
+++ automated_api_tesing/framework_api_testing_final/api_testing/api_method.py
@@ -8,7 +8,8 @@ import os
 #from resourses.resourse import file_location,reader,row_count,out_file,data,update
 import resourses.resourse
 
-input_file_number =0
+input_file_number = 0
+response_list =[]
 
 #first input file
 api_name ="input_cms.csv"
@@ -18,21 +19,28 @@ api_name ="input_cms.csv"
 #######################################
 def get_method(url,parameter, header, body, response,i):
     print("This is get request")
-    if(isinstance(header,str)):
+    if(isinstance(header,str) and header!=None):
         header = json.loads(header)
 
-    if (isinstance(parameter, str) and parameter != None):
+    if (isinstance(parameter, str)):
         parameter = json.loads(parameter)
 
     response_after_get = requests.get(url,params=parameter,headers = header)
+    if (response_after_get.headers['content-type'] =='text/html; charset=utf-8'):
+        print("pass")
+        print(response_after_get)
+    else:
+        global response_list
+        response_list = response_after_get.json()
+        print(response_list)
 
-    response_list = response_after_get.json()
-    print(response_list)
+
+
     print(response_after_get.status_code )
     if(response_after_get.status_code == 200):
-        status_get = "get response successfully"
+        status_get = "PASS"
     else:
-        status_get = "fail to get response"
+        status_get = "FAIL"
 
     #writing status code
     resourses.resourse.update(i,response_after_get.status_code,status_get,response_list)
@@ -53,14 +61,16 @@ def post_method(url,parameter, header, body, response,i):
         parameter = json.loads(parameter)
 
     response_after_post = requests.post(url,body,params= parameter,headers = header)
+
+
     response_list = response_after_post.json()
     print(response_list)
     print(response_after_post.status_code)
     #checking status code
     if(response_after_post.status_code == 201 or response_after_post.status_code == 200):
-        status_post = "posted succussfully"
+        status_post = "PASS"
     else:
-        status_post = "fail to post"
+        status_post = "FAIL"
 
     #writing status code
     resourses.resourse.update(i,response_after_post.status_code,status_post,response_list)
@@ -80,7 +90,7 @@ def post_method_login(url,parameter, header, body, response,i):
     if(isinstance(header, str)):
         header = json.loads(header)
 
-    if (isinstance(parameter, str) and parameter != None):
+    if (parameter != None and isinstance(parameter, str) ):
         parameter = json.loads(parameter)
 
     response_after_post = requests.post(url,body,params = parameter,headers = header)
@@ -90,16 +100,14 @@ def post_method_login(url,parameter, header, body, response,i):
     #this token will recieve after login
     #extracting the token from response header
     token_recieve_from_header_response= response_after_post.headers['Authorization']
-    body_dict = json.loads(body)
-    user_Id = body_dict['emailId']
-    header_containing_token = {'token': token_recieve_from_header_response, 'userId': user_Id}
+
 
     print(response_after_post.status_code)
     #checking status code
     if(response_after_post.status_code == 201 or response_after_post.status_code == 200):
-        status_post = "posted succussfully"
+        status_post = "PASS"
     else:
-        status_post = "fail to post"
+        status_post = "FAIL"
 
     resourses.resourse.update(i,response_after_post.status_code,status_post,response_list)
 
@@ -115,6 +123,13 @@ def post_method_login(url,parameter, header, body, response,i):
         method = resourses.resourse.data[i][2]
         parameter = resourses.resourse.data[i][3]
         if(api_name=="input_cms.csv" ):
+            # converting header string to dict format
+            if (isinstance(resourses.resourse.data[i][4], str)):
+                resourses.resourse.data[i][4] = json.loads(resourses.resourse.data[i][4])
+
+            user_Id = resourses.resourse.data[i][4]["userId"]
+
+            header_containing_token = {'token': token_recieve_from_header_response, 'userId': user_Id}
             header = header_containing_token
         else:
             header = resourses.resourse.data[i][4]
@@ -132,11 +147,13 @@ def post_method_login(url,parameter, header, body, response,i):
             delete_method(url,parameter, header, body, response, i)
 
         input_file_list = os.listdir("C:/Users/SONY/PycharmProjects/framework_api_testing_final/resourses")
+
+        #sort all the file
+        input_file_list.sort()
+
         #removing the pycache file that is not necessary
         if "__pycache__" in input_file_list:
             input_file_list.remove('__pycache__')
-
-        print(input_file_list,"**********")
 
         #total number of file in resourse folder
         number_of_input_file = len(input_file_list)
@@ -159,6 +176,8 @@ def post_method_login(url,parameter, header, body, response,i):
             input_file_number += 1
             s = api_name
             print("Reading from "+s[6:])
+
+            #read_write function is in resourse.py file inside resourse folder
             resourses.resourse.read_write(api_name, "output" +s[5:] )
 
 
@@ -183,9 +202,9 @@ def put_method(url,parameter, header, body, response,i):
 
     #checking status code
     if(response_after_put.status_code == 200):
-        status_put = "modified succfully"
+        status_put = "PASS"
     else:
-        status_put = "fail to modify"
+        status_put = "FAIL"
     #print(status_put)
     resourses.resourse.update(i,response_after_put.status_code,status_put,response_list)
 
@@ -210,9 +229,9 @@ def delete_method(url,parameter, header, body, response,i):
     # fetch responce code
     print(response_after_delete.status_code)
     if(response_after_delete.status_code == 204 or response_after_delete.status_code == 200):
-        status_delete = "successfully deleted"
+        status_delete = "PASS"
     else:
-        status_delete = "fail to delete"
+        status_delete = "FAIL"
     #writing status code
     resourses.resourse.update(i,response_after_delete.status_code,status_delete,response_list)
 
