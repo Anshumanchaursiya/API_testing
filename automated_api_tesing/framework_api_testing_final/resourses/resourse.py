d2283e3d691c7e78d28ef2a701befb264f78f211
Anshumanchaursiya <ansh9010@gmail.com>
2019-06-10 01:16:35 -0700

    os.listdir sorted order

--- automated_api_tesing/framework_api_testing_final/resourses/resourse.py
+++ automated_api_tesing/framework_api_testing_final/resourses/resourse.py
@@ -8,11 +8,13 @@ CSVWriter = ''
 row_count = 0
 in_file = ''
 out_file = ''
+import codecs
 def read_write(input_file, output_file):
     global in_file
     in_file = "C:/Users/SONY/PycharmProjects/framework_api_testing_final/resourses/" + input_file
 
-    with open(in_file, "r") as f:
+    with open(in_file, "r",encoding='utf-8') as f:
+        #reader = csv.reader(codecs.open(f, 'rU', 'utf-16'))
         reader = csv.reader(f, delimiter=',')
         global data
         data = list(reader)
@@ -27,7 +29,7 @@ def read_write(input_file, output_file):
     global CSVWriter
     CSVWriter = csv.writer(out_file)
 
-    CSVWriter.writerow(["*SERIAL NO.*", "***URL***", "***REQUEST TYPE***","***QUERY PARAMETER***" "***HEADER***", "*** REQUEST BODY***","***EXPECTED STATUS CODE***", "***ACTUAL STATUS CODE***", "***VALIDATION***", "***RESPONSE***"])
+    CSVWriter.writerow(["*SERIAL NO.*", "***URL***", "***REQUEST TYPE***","***QUERY PARAMETER***","***HEADER***", "*** REQUEST BODY***","***EXPECTED STATUS CODE***", "***ACTUAL STATUS CODE***", "***VALIDATION***", "***RESPONSE***"])
 
 
 
