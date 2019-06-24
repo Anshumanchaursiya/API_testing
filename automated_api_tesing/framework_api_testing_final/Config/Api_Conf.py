368bf295d87ddfa4e30825d5d2dd3e902b86fd5b
Munikrishna M <munikrishna.mothukuru@datami.com>
2019-06-24 15:51:22 +0530

    adding configuration file

---
+++ automated_api_tesing/framework_api_testing_final/Config/Api_Conf.py
@@ -0,0 +1,10 @@
+#This file is for the initial configuration required to run the framework
+
+#input file path
+INPUT_FILE_PATH = "/Users/datami/PycharmProjects/framework_api_testing/resourses/"
+
+#output file path
+OUTPUT_FILE_PATH = "/Users/datami/PycharmProjects/framework_api_testing/out/"
+
+#set time how much you want to wait for the response
+SETTIME = 3
